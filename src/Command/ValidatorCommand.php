03579c1e8df97b66583854c0730b226244243ccd
Mateusz Durmaj <m.durmaj@gmail.com>
2019-02-18 14:50:16 +0100

    init commit

---
+++ src/Command/ValidatorCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use App\Service\EmailValidatorService;
+
+class ValidatorCommand extends Command
+{
+
+    protected static $defaultName = 'validate:email';
+    private $emailValidator;
+
+    public function __construct(EmailValidatorService $emailValidatorService)
+    {
+        $this->emailValidator = $emailValidatorService;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Checks the validity of e-mail addresses provided in CSV file')
+             ->setHelp('This command checks the validity of e-mail addresses provided in CSV file');
+
+        $this->addArgument('filename', InputArgument::REQUIRED, 'The name of CSV file with e-mail addresses');
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'Starting e-mail validator'
+        ]);
+
+        $csvFileName = $input->getArgument('filename');
+
+        $result = $this->emailValidator->checkEmails($csvFileName);
+
+        $output->writeln([$result]);
+
+    }
+}
---
+++ src/Service/EmailValidatorService.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Service;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EmailValidatorService
+{
+
+    public function checkEmails($csvFileName){
+
+        $csvFile =
+
+
+        return 'ok';
+
+    }
+
+}
